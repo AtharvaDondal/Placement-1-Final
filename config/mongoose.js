88a67a20d8b2b988c0b41632d99176896e3a5720
AtharvaDondal <120319760+AtharvaDondal@users.noreply.github.com>
2024-07-30 09:22:01 +0530

    adding some changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,11 @@
 // const mongoose = require('mongoose');
+
 // async function main(){
 //     await mongoose.connect('mongodb+srv://placement:1UbhMXi13RXuCwSn@cluster0.p5gbb.mongodb.net/placementcell?retryWrites=true&w=majority');
 //     console.log("connection Successfull !! ");
 // }
-// main().catch(error =>console.log("connection not successfull !!"));
-// const mongoose = require("mongoose");
+// main().catch(error =>console.log(error,"connection not successfull !!"));
+// // const mongoose = require("mongoose");
 // mongoose.set('strictQuery', false);
 // const mongoDB = "mongodb+srv://user1:Mirzapur@1@cluster0.nqv5wfv.mongodb.net/?retryWrites=true&w=majority"; 
 // main().catch(err => console.log(err));
@@ -17,8 +18,9 @@
 
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-const mongoDB = "mongodb+srv://user1:user1@cluster0.gaqmv3e.mongodb.net/PLACEMENT?retryWrites=true&w=majority"; 
+const mongoDB = "mongodb://localhost:27017"; 
 main().catch(err => console.log(err));
+
 async function main() {
   await mongoose.connect(mongoDB);
   console.log('connected');
